19042417c6262de9dcc0724909bbb2958179e936
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2020-04-01 13:27:12 +0200

    Initial iOS project

---
+++ Hamburger-Menu-SwiftUITests/Hamburger_Menu_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Hamburger_Menu_SwiftUITests.swift
+//  Hamburger-Menu-SwiftUITests
+//
+//  Created by Przemyslaw Pajak on 01/04/2020.
+//  Copyright © 2020 FEARLESS SPIDER. All rights reserved.
+//
+
+import XCTest
+@testable import Hamburger_Menu_SwiftUI
+
+class Hamburger_Menu_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
